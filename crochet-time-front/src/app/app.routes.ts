042a5a4ab39f83f27823febf8b41654dca7ecf78
Candice Dutournier <candice.dutournier2@gmail.com>
2026-01-12 12:10:42 +0100

    modif d'une limite dans laravel (prblmancienne version bdd) et ajout de nouveaux composants angular

--- crochet-time-front/src/app/app.routes.ts
+++ crochet-time-front/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { AccueilComponent } from './components/accueil/accueil.component';
+import { PanierComponent } from './components/panier/panier.component';
+import { AdminComponent } from './components/admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: 'accueil', 
+        component: AccueilComponent
+    },
+    { 
+        path: 'panier', 
+        component: PanierComponent
+    },
+    { 
+        path: 'admin', 
+        component: AdminComponent
+    },
+    { path: '', redirectTo: 'accueil', pathMatch: 'full' }
+];
---
+++ crochet-time-front/src/app/components/accueil/accueil.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accueil',
+  imports: [],
+  templateUrl: './accueil.component.html',
+  styleUrls: ['./accueil.component.css']
+})
+export class AccueilComponent {
+
+}
